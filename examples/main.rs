091e97fd5e00d8fa97d6f790302011ae29e61f03
Lorenzo Fontoura <lorenzo@nioel.com>
2023-07-18 12:04:39 +1000

    chore: add main example

--- examples/main.rs
+++ examples/main.rs
@@ -1,4 +1,9 @@
+use bigdecimal::{BigDecimal, FromPrimitive};
+use bigdecimal_ethers_ext::BigDecimalEthersExt;
 
 fn main() {
-
+    let big_decimal = BigDecimal::from_f64(10.5).unwrap();
+    // Convert to an u256 with 18 decimals.
+    let u256 = big_decimal.to_ethers_u256(18).unwrap();
+    println!("{u256}");
 }
