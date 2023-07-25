b583564f1fb31b4fa3aa9f0c78a4ed20f6449f56
Lorenzo Fontoura <lorenzo@nioel.com>
2023-07-25 12:24:57 +1000

    fix: use left pad

--- src/from_ethers.rs
+++ src/from_ethers.rs
@@ -1,24 +1,22 @@
-use crate::BigDecimalEthersExt;
 use bigdecimal::{BigDecimal, Num};
 use ethers::prelude::{I256, U256};
 
-pub(crate) fn from_ethers_u256(bn: U256, decimals: u8) -> Option<BigDecimal> {
+pub(crate) fn from_ethers_u256(bn: &U256, decimals: u8) -> Option<BigDecimal> {
     to_big_decimal(bn.to_string(), decimals)
 }
 
-pub(crate) fn from_ethers_i256(bn: I256, decimals: u8) -> Option<BigDecimal> {
+pub(crate) fn from_ethers_i256(bn: &I256, decimals: u8) -> Option<BigDecimal> {
     to_big_decimal(bn.to_string(), decimals)
 }
 
-fn to_big_decimal(bn_string: String, decimals: u8) -> Option<BigDecimal> {
+fn to_big_decimal(string: String, decimals: u8) -> Option<BigDecimal> {
     let decimals = decimals as usize;
-    let Some(mut value_le) = reversed_string(bn_string) else {
-        return None;
-    };
     // e.g. for bn_str = "0", decimals = 1; must pad to "00" and return "0.0".
-    right_pad(&mut value_le, decimals + 1, '0');
-    let decimal_part = &value_le[0..decimals];
-    let integer_part = &value_le[decimals..decimals + 1];
+    let string = left_pad(string, decimals + 1, '0');
+    let len = string.len();
+    let decimal_start_index = len - decimals;
+    let decimal_part = &string[decimal_start_index..];
+    let integer_part = &string[..decimal_start_index];
     let complete = format!("{integer_part}.{decimal_part}");
     let Ok(bd) = BigDecimal::from_str_radix(&complete, 10) else {
         return None
@@ -26,16 +24,27 @@ fn to_big_decimal(bn_string: String, decimals: u8) -> Option<BigDecimal> {
     Some(bd)
 }
 
-fn reversed_string(str: String) -> Option<String> {
-    let mut bytes = str.into_bytes();
-    bytes.reverse();
-    String::from_utf8(bytes)
-        .map(|str| Some(str))
-        .unwrap_or(None)
+fn left_pad(string: String, length: usize, fill: char) -> String {
+    let diff: isize = (length as isize) - (string.len() as isize);
+    if diff <= 0 {
+        return string;
+    }
+    let bytes = vec![fill as u8; diff as usize];
+    let fill_string = String::from_utf8(bytes).unwrap();
+    format!("{fill_string}{string}")
 }
 
-fn right_pad(string: &mut String, length: usize, fill: char) {
-    while string.len() < length {
-        string.push(fill);
+#[cfg(test)]
+mod test {
+    use crate::BigDecimalEthersExt;
+    use bigdecimal::BigDecimal;
+    use ethers::types::U256;
+
+    #[test]
+    fn should_convert_from_ethers_u256() {
+        let u256 = U256::from_str_radix("1005", 10).unwrap();
+        let bd = BigDecimal::from_ethers_u256(&u256, 2).unwrap();
+        let bd_string = bd.to_string();
+        assert_eq!(bd_string, "10.05");
     }
 }
