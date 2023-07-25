83a6afc8ef40c007f9f4ff04f643ccf98e8c1a69
Lorenzo Fontoura <lorenzo@nioel.com>
2023-07-25 12:48:18 +1000

    chore: write from example

--- examples/main.rs
+++ examples/main.rs
@@ -7,4 +7,8 @@ fn main() {
     let u256 = big_decimal.to_ethers_u256(18).unwrap();
     println!("{u256}");
     // 10500000000000000000
+    // Convert from an u256 with 18 decimals to a BigDecimal.
+    let to_big_decimal = BigDecimal::from_ethers_u256(&u256, 18).unwrap();
+    println!("{to_big_decimal}");
+    // 10.500000000000000000
 }
