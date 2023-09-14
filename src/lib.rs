2500f56fe0a1e2c079c860a1eebe64fdf0ecc2b9
Lorenzo Fontoura <lorenzo@nioel.com>
2023-09-14 12:47:46 +1000

    chore: bump version

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 mod from_ethers;
 mod to_ethers;
-use crate::from_ethers::{from_ethers_i256, from_ethers_u256};
+use crate::from_ethers::{from_ethers_i256, from_ethers_u256, to_big_decimal};
 use crate::to_ethers::{to_ethers_i256, to_ethers_u256};
 use bigdecimal::BigDecimal;
 use ethers::types::{I256, U256};
@@ -13,6 +13,7 @@ where
     fn to_ethers_i256(&self, decimals: u8) -> Option<I256>;
     fn from_ethers_u256(bn: &U256, decimals: u8) -> Option<Self>;
     fn from_ethers_i256(bn: &I256, decimals: u8) -> Option<Self>;
+    fn from_bn_string(bn: String, decimals: u8) -> Option<Self>;
 }
 
 impl BigDecimalEthersExt for BigDecimal {
@@ -30,5 +31,9 @@ impl BigDecimalEthersExt for BigDecimal {
 
     fn from_ethers_i256(bn: &I256, decimals: u8) -> Option<Self> {
         from_ethers_i256(bn, decimals)
+    }
+
+    fn from_bn_string(bn: String, decimals: u8) -> Option<Self> {
+        to_big_decimal(bn, decimals)
     }
 }
